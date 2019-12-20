c253ad770d4f984cca0d177bf5d214f027aaadcd
Jeyson Orrego <jeysonorrego@qdata.io>
2019-12-20 09:30:47 -0500

    [HUB-16243] create http request module

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { URLSearchParams } from '@angular/http';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
 import { Request, Response } from "./http.service.model";
 
 @Injectable({
@@ -23,7 +25,35 @@ export class HttpService {
     return this.http.get<Response>(input.path);
   }
 
+  public post(input: Request) {
+    this.http.post<Response>(input.path, input.data).pipe(
+      // catchError(this.handleError(''))
+    );
+  }
+
+  public put(input: Request) {
+    this.http.put<Response>(input.path, input.data).pipe(
+      // catchError(this.handleError(''))
+    );
+  }
+
   public preload(flag: boolean) {
     console.log('preload', flag);
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 }
