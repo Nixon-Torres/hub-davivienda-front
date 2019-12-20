5a59bb1ca4cb3216f828a63ec90308359b370a8b
liliana <>
2019-12-20 16:15:52 -0500

    [master] add reset-css module, include assets

--- src/app/pages/private/principal/principal.module.ts
+++ src/app/pages/private/principal/principal.module.ts
@@ -1,20 +1,36 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { LayoutModule } from '@angular/cdk/layout';
 import { PrincipalComponent } from './principal.component';
 
 import { Routes, RouterModule } from '@angular/router';
+import { LeftBarComponent } from './left-bar/left-bar.component';
+import { RightContentComponent } from './right-content/right-content.component';
+
+import { MatTabsModule } from '@angular/material/tabs';
+
+
 const routes: Routes = [
   {
-    path: '',
-    component: PrincipalComponent
+	path: '',
+	component: PrincipalComponent
   }
 ];
 
+
+const materialModules: any[] = [
+	MatTabsModule
+];
+
 @NgModule({
-  declarations: [PrincipalComponent],
+  declarations: [PrincipalComponent, LeftBarComponent, RightContentComponent],
   imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
+	CommonModule,
+	materialModules,
+	RouterModule.forChild(routes)
+  ],
+  exports: [
+	materialModules
   ]
 })
 export class PrincipalModule { }
