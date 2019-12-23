101742e87cc3379b55deaf50af2e83e3aba13425
Jeyson Orrego <jeysonorrego@qdata.io>
2019-12-23 14:59:26 -0500

    [HUB-16243] load enviromnet url api

--- src/app/services/http.service.model.ts
+++ src/app/services/http.service.model.ts
@@ -1,11 +1,15 @@
 export class Request {
-  path: string;
-  data: any;
-  headers: Array<any> = [];
+    path: string;
+    data?: any;
+    headers?: Array<any> = [];
 }
 
 export interface Response {
-  success: string;
-  data: any;
-  message: string;
+    headers: any;
+    status: number;
+    statusText: string;
+    url: string;
+    ok: boolean;
+    type: number;
+    body: any;
 }
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,59 +1,71 @@
 import { Injectable } from '@angular/core';
 import { URLSearchParams } from '@angular/http';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { throwError } from 'rxjs';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
 import { Request, Response } from "./http.service.model";
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class HttpService {
+    private _URL_API: string = environment.URL_API;
 
-  constructor(
-    private http: HttpClient
-  ) { }
+    constructor(
+        private http: HttpClient
+    ) { }
 
-  public get(input: Request) {
-    let params = new URLSearchParams();
-    for (let key in input.data) {
-      if (input.data.hasOwnProperty(key)) {
-        params.set(key, input.data[key])
-      }
+    public get(input: Request): Observable<HttpResponse<Response>> {
+        let params = new URLSearchParams();
+        for (let key in input.data) {
+            if (input.data.hasOwnProperty(key)) {
+                params.set(key, input.data[key])
+            }
+        }
+        return this.http.get<Response>(this._URL_API + input.path, {
+            observe: 'response'
+        }).pipe(catchError(this.handleError));
     }
-    console.log(params);
-    return this.http.get<Response>(input.path);
-  }
 
-  public post(input: Request) {
-    this.http.post<Response>(input.path, input.data).pipe(
-      // catchError(this.handleError(''))
-    );
-  }
-
-  public put(input: Request) {
-    this.http.put<Response>(input.path, input.data).pipe(
-      // catchError(this.handleError(''))
-    );
-  }
-
-  public preload(flag: boolean) {
-    console.log('preload', flag);
-  }
-
-  private handleError(error: HttpErrorResponse) {
-    if (error.error instanceof ErrorEvent) {
-      // A client-side or network error occurred. Handle it accordingly.
-      console.error('An error occurred:', error.error.message);
-    } else {
-      // The backend returned an unsuccessful response code.
-      // The response body may contain clues as to what went wrong,
-      console.error(
-        `Backend returned code ${error.status}, ` +
-        `body was: ${error.error}`);
+    public post(input: Request): Observable<HttpResponse<Response>> {
+        return this.http.post<Response>(input.path, input.data, {
+            observe: 'response'
+        }).pipe(catchError(this.handleError));
     }
-    // return an observable with a user-facing error message
-    return throwError(
-      'Something bad happened; please try again later.');
-  };
+
+    public put(input: Request): Observable<HttpResponse<Response>> {
+        return this.http.put<Response>(input.path, input.data, {
+            observe: 'response'
+        }).pipe(catchError(this.handleError));
+    }
+
+    public delete(input: Request): Observable<HttpResponse<Response>> {
+        let params = new URLSearchParams();
+        for (let key in input.data) {
+            if (input.data.hasOwnProperty(key)) {
+                params.set(key, input.data[key])
+            }
+        }
+        return this.http.delete<Response>(input.path, {
+            observe: 'response'
+        }).pipe(catchError(this.handleError));
+    }
+
+    public preload(flag: boolean) {
+        console.log('preload', flag);
+    }
+
+    private handleError(error: HttpErrorResponse) {
+        if (error.error instanceof ErrorEvent) {
+            console.error('An error occurred:', error.error.message);
+        } else {
+            console.error(
+                `Backend returned code ${error.status}, body was: `,
+                error.error
+            );
+        }
+        return throwError('Something bad happened; please try again later.');
+    };
 }
