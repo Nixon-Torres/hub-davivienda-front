8c99ecb6619dede7fce19dc318ecb4e3ca4f712e
Liliana Fabiola Meléndez Alvarez <lilianamelendez@qdata.io>
2019-12-23 16:48:00 -0500

    [HUB-15848] add modal box for create edit and delete folders

--- src/app/pages/private/principal/left-bar/left-bar.component.ts
+++ src/app/pages/private/principal/left-bar/left-bar.component.ts
@@ -1,30 +1,47 @@
+
 import { Component, OnInit } from '@angular/core';
 import { HttpService } from '../../../../services/http.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { DialogBoxComponent } from '../dialog-box/dialog-box.component';
+
 
 @Component({
-    selector: 'app-left-bar',
-    templateUrl: './left-bar.component.html',
-    styleUrls: ['./left-bar.component.scss']
+	selector: 'app-left-bar',
+	templateUrl: './left-bar.component.html',
+	styleUrls: ['./left-bar.component.scss']
 })
 export class LeftBarComponent implements OnInit {
-    public list: any = {
-        folders: []
-    }
+	public list: any = {
+		folders: []
+	}
 
-    constructor(
-        private http: HttpService
-    ) { }
+	constructor(
+		private http: HttpService,
+		public dialog: MatDialog
+	) { }
 
-    ngOnInit() {
-        this.loadFolders();
-    }
+	openDialog(): void {
+		const dialogRef = this.dialog.open(DialogBoxComponent, {
+			width: '600px',
+			height: 'auto',
+			maxHeight: '500px'
+		});
 
-    private loadFolders() {
-        this.http.get({
-            'path': 'folders'
-        }).subscribe((response) => {
-            this.list.folders = response.body;
-        });
-    }
+		dialogRef.afterClosed().subscribe(result => {
+			console.log('The dialog was closed');
+		});
+  	} 
+
+	ngOnInit() {
+		this.loadFolders();
+	}
+
+	private loadFolders() {
+		this.http.get({
+			'path': 'folders'
+		}).subscribe((response) => {
+			this.list.folders = response.body;
+		});
+	}
 
 }
--- src/app/pages/private/principal/principal.module.ts
+++ src/app/pages/private/principal/principal.module.ts
@@ -8,29 +8,57 @@ import { LeftBarComponent } from './left-bar/left-bar.component';
 import { RightContentComponent } from './right-content/right-content.component';
 
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatDialogModule } from '@angular/material/dialog';
+// Form Controls
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+// Buttons & Indicators
+import { MatButtonModule } from '@angular/material/button';
+
+
+import { DialogBoxComponent } from './dialog-box/dialog-box.component';
 
 
 const routes: Routes = [
-  {
-	path: '',
-	component: PrincipalComponent
-  }
+	{
+		path: '',
+		component: PrincipalComponent
+	}
 ];
 
-
 const materialModules: any[] = [
-	MatTabsModule
+	MatTabsModule,
+	MatDialogModule,
+	MatAutocompleteModule,
+	MatCheckboxModule,
+	MatDatepickerModule,
+	MatFormFieldModule,
+	MatInputModule,
+	MatRadioModule,
+	MatSliderModule,
+	MatSlideToggleModule,
+	MatButtonModule
 ];
 
 @NgModule({
-  declarations: [PrincipalComponent, LeftBarComponent, RightContentComponent],
-  imports: [
-	CommonModule,
-	materialModules,
-	RouterModule.forChild(routes)
-  ],
-  exports: [
-	materialModules
-  ]
+	declarations: [PrincipalComponent, LeftBarComponent, RightContentComponent, DialogBoxComponent],
+	imports: [
+		CommonModule,
+		materialModules,
+		RouterModule.forChild(routes)
+	],
+	exports: [
+		materialModules
+	],
+	entryComponents: [
+		DialogBoxComponent
+	]
 })
 export class PrincipalModule { }
