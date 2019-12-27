cc652ad6b322804be1e667d98f772767cb24d361
Liliana Fabiola Meléndez Alvarez <lilianamelendez@qdata.io>
2019-12-27 13:16:14 -0500

    [HUB-15857] show actions menu within report list

--- src/app/pages/private/principal/right-content/right-content.component.ts
+++ src/app/pages/private/principal/right-content/right-content.component.ts
@@ -6,44 +6,79 @@ import { CreateReportDialogComponent } from '../create-report-dialog/create-repo
 import { Router } from '@angular/router';
 
 @Component({
-    selector: 'app-right-content',
-    templateUrl: './right-content.component.html',
-    styleUrls: ['./right-content.component.scss']
+	selector: 'app-right-content',
+	templateUrl: './right-content.component.html',
+	styleUrls: ['./right-content.component.scss']
 })
 export class RightContentComponent implements OnInit {
-    public list: any = {
-        'reports': []
-    }
+	public list: any = {
+		'reports': []
+	}
 
-    constructor(
-        public dialog: MatDialog,
-        private router: Router,
-        private http: HttpService
-    ) { }
+	constructor(
+		public dialog: MatDialog,
+		private router: Router,
+		private http: HttpService
+	) { }
 
-    openDialog(): void {
-        const dialogRef = this.dialog.open(CreateReportDialogComponent, {
-            width: '1500px'
-        });
+	openDialog(): void {
+		const dialogRef = this.dialog.open(CreateReportDialogComponent, {
+			width: '1500px'
+		});
 
-        dialogRef.afterClosed().subscribe((result: any) => {
-            console.log('The dialog was closed', result);
-        });
-    }
+		dialogRef.afterClosed().subscribe((result: any) => {
+			console.log('The dialog was closed', result);
+		});
+	}
 
-    ngOnInit() {
-        this.loadReports();
-    }
+	ngOnInit() {
+		this.loadReports();
+	}
 
-    private loadReports(): void {
-        this.http.get({
-            'path': 'reports?filter[include][0][relation]=folder&filter[include][1][relation]=user&filter[include][2][relation]=state&filter[include][3][relation]=section'
-        }).subscribe((response) => {
-            this.list.reports = response.body;
-        });
-    }
+	private loadReports(): void {
+		this.http.get({
+			'path': 'reports?filter[include][0][relation]=folder&filter[include][1][relation]=user&filter[include][2][relation]=state&filter[include][3][relation]=section'
+		}).subscribe((response) => {
+			this.list.reports = response.body;
+		});
+	}
 
-    public gotoPage(input: string) {
-        this.router.navigate(['app/board', input]);
-    }
+	private saveReport(clone): void {
+		this.http.post({
+			'path': 'reports',
+			'data': clone
+		}).subscribe((response) => {
+			console.log(response);
+		});
+	}
+
+	public gotoPage(input: string) {
+		this.router.navigate(['app/board', input]);
+	}
+
+	public onCloneReport(event: Event, pos: number) {
+
+		let clone = Object.assign({} , this.list.reports[pos]);
+		clone.name = clone.name + ' Copia';
+		clone.slug = clone.slug +'-copia';
+		clone.id = this.getId();
+		this.list.reports.splice(pos+1,0,clone);
+
+		//this.saveReport(clone);
+	}
+
+	public onDeleteReport(event: Event, pos: number) {
+		this.list.reports.splice(pos,1);
+	}
+
+	public getId () {
+		var str = '';
+		var ref = 'abcdefghijklmnopqrstuvwxyz0123456789';
+		for (var i=0; i<24; i++)
+			{
+				str += ref.charAt(Math.floor(Math.random()*ref.length));
+			}
+		return str;	
+	}
+
 }
