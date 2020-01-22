b541eff8b499495663c88f7de327bc697995c802
Jhosseeth Naranjo <jhosseethnaranjo@qdata.io>
2020-01-22 12:21:00 -0500

    [HUB-16813F] Include state report relationship

--- src/app/pages/private/board/board.component.ts
+++ src/app/pages/private/board/board.component.ts
@@ -106,7 +106,16 @@ export class BoardComponent implements OnInit, AfterViewInit {
     * @return { this.report } Obj with the report data
     */
     private loadReport(idReport: string): void {
+
         var query = new loopback();
+
+        query.filter.include.push({
+            relation: "state",
+            scope: {
+                fields: ['name']
+            }
+        });
+
         query.filter.include.push({
             relation: "events",
             scope: {
@@ -126,10 +135,10 @@ export class BoardComponent implements OnInit, AfterViewInit {
             'data': query.filter,
             'encode': true
         }).subscribe((response: any) => {
+
             response.body.folderId = response.body.folderId ? response.body.folderId : null;
             response.body.templateId = response.body.templateId ? response.body.templateId : null;
             this.report = response.body;
-
             this.setLastUpdate(response.body.updatedAt);
 
             setTimeout(() => {
@@ -286,7 +295,7 @@ export class BoardComponent implements OnInit, AfterViewInit {
             'data': {
                 reportId: this.report.id,
                 reviewers: this.getReviewers(reviewers)
-            } 
+            }
         }).subscribe( (resp) => {
             if(resp) {
                 this.dialog.open(ConfirmationDialogComponent, {
