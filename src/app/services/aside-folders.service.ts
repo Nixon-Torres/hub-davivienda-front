6fcf1003b84644c8ad700326eaa2b912daf83d8a
Jeyson Orrego <jeysonorrego@qdata.io>
2020-01-22 10:48:40 -0500

    [HUB-16808] change qs to encode

--- src/app/services/aside-folders.service.ts
+++ src/app/services/aside-folders.service.ts
@@ -1,87 +1,82 @@
 import { Injectable } from '@angular/core';
 import { HttpService } from './http.service';
 import { loopback } from '../models/common/loopback.model';
-import * as qs from 'qs';
 import { Subject } from 'rxjs';
 
-
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class AsideFoldersService {
-
-
-  private list: any = {
-    folders: [],
-    states: []
-  }
-
-  public listenFolders;
-  public $listenFolders;
-  public foldersSubs;
-  public listenStates;
-  public $listenStates;
-  public statesSubs;
-  
-  constructor(private http: HttpService) { 
-    console.log('aside-service');
-    this.listenFolders = new Subject();
-    this.listenStates = new Subject();
-    this.$listenFolders = this.listenFolders.asObservable();
-    this.$listenStates = this.listenStates.asObservable();
-    this.loadStates();
-    this.loadFolders();
-    
-  }
-  
-  private loadFolders() {
-    var query = new loopback();
-    query.filter.include.push({ relation: "reports", scope: {where: {trash: false }}});
-    console.log('query folders',JSON.stringify(qs.parse(qs.stringify(query,{skipNulls: true }))));
-    
-    this.http.get({
-      path: 'folders?'+qs.stringify(query,{skipNulls: true })
-    }).subscribe((response) => {
-      this.list.folders = this.updateFolders(response.body);
-    });
-  }
-  private loadStates() {
-    var query = new loopback();
-    query.filter.include.push({ relation: "reports", scope: {where: {trash: false }}});
-    console.log('query states', JSON.stringify(qs.parse(qs.stringify(query,{skipNulls: true }))));
-
-    this.http.get({
-        path: 'states?'+qs.stringify(query,{skipNulls: true })
-    }).subscribe((response) => {
-        this.list.states = this.updateStates(response.body);
-    });
-  }
-
-  private updateFolders(folder) {
-    if(folder) {
-        this.listenFolders.next(folder);
+    private list: any = {
+        folders: [],
+        states: []
     }
-  }
-  private updateStates(states) {
-    if(states) {
-        this.listenStates.next(states);
+
+    public listenFolders;
+    public $listenFolders;
+    public foldersSubs;
+    public listenStates;
+    public $listenStates;
+    public statesSubs;
+
+    constructor(private http: HttpService) {
+        console.log('aside-service');
+        this.listenFolders = new Subject();
+        this.listenStates = new Subject();
+        this.$listenFolders = this.listenFolders.asObservable();
+        this.$listenStates = this.listenStates.asObservable();
+        this.loadStates();
+        this.loadFolders();
     }
-  }
 
-  get folderList() {
-    return this.list.folders;
-  }
+    private loadFolders() {
+        var query = new loopback();
+        query.filter.include.push({ relation: "reports", scope: { where: { trash: false }, type: "count" } });
+        this.http.get({
+            path: 'folders',
+            data: query.filter,
+            encode: true
+        }).subscribe((response) => {
+            this.list.folders = this.updateFolders(response.body);
+        });
+    }
+    private loadStates() {
+        var query = new loopback();
+        query.filter.include.push({ relation: "reports", scope: { where: { trash: false } } });
+        this.http.get({
+            path: 'states',
+            data: query.filter,
+            encode: true
+        }).subscribe((response) => {
+            this.list.states = this.updateStates(response.body);
+        });
+    }
 
-  set folderList(value: Array<any>) {
-    this.list.folder = value;
-  }
+    private updateFolders(folder: any) {
+        if (folder) {
+            this.listenFolders.next(folder);
+        }
+    }
+    private updateStates(states: any) {
+        if (states) {
+            this.listenStates.next(states);
+        }
+    }
 
-  get stateList() {
-    return this.list.states;
-  }
+    get folderList() {
+        return this.list.folders;
+    }
 
-  set stateList (value: Array<any>) {
-    this.list.states = value;
-  }
+    set folderList(value: Array<any>) {
+        this.list.folder = value;
+    }
+
+    get stateList() {
+        return this.list.states;
+    }
+
+    set stateList(value: Array<any>) {
+        this.list.states = value;
+    }
 
 }
