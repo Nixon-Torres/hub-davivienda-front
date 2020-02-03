a2df0ff1eafb00c543322ade57a76b5c2b18cd42
Jeyson Orrego <jeysonorrego@qdata.io>
2020-02-03 16:48:39 -0500

    [HUB-16982] create sockets service

--- src/app/services/socket.service.ts
+++ src/app/services/socket.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
-import { environment } from '../../environments/environment';
 import { AuthService } from './auth.service';
+import { environment } from '../../environments/environment';
 
 import io from 'socket.io-client';
 
@@ -10,28 +11,71 @@ import io from 'socket.io-client';
 })
 export class SocketService {
     private _URL_SOCKET = environment.URL_SOCKET;
-    private socket: any;
+    private socket: any = null;
 
     constructor(
         private auth: AuthService
-    ) {
-        this.startIo();
+    ) { }
+
+    public on(nsp: string): Observable<any> {
+        return new Observable((observer: Observer<any>) => {
+            this.socket.on(nsp, (response: any) => {
+                observer.next(response);
+            });
+        });
     }
 
-    private startIo(): void {
-        if (!this.auth.isLoggedin()) return;
+    public emit(nsp: string, data?: any): Observable<any> {
+        data = data || {};
+        return new Observable((observer: Observer<any>) => {
+            this.socket.emit(nsp, data, (response: any) => {
+                observer.next(response);
+            });
+        });
+    }
+
+    public start(): Observable<any> {
+        return new Observable((observer: Observer<any>) => {
+            if (!this.auth.isLoggedin()) {
+                observer.error('Error: forbiden');
+                observer.complete();
+                return;
+            };
+            this.connect((isConnected: boolean) => {
+                this.authenticate();
+                this.on('authenticated').subscribe(
+                    (isAuthenticated: boolean) => observer.next(isConnected && isAuthenticated)
+                );
+            });
+        });
+    }
+
+    public removeListener(nsp: string): Observable<any> {
+        return new Observable((observer: Observer<any>) => {
+            this.socket.off(nsp, (response: any) => {
+                observer.next(response);
+            });
+        });
+    }
+
+    private connect(fn: Function): void {
+        if (!this.socket) {
+            this.socket = io(this._URL_SOCKET);
+            this.socket.on('connect', () => fn(true));
+        } else if (this.socket && !this.socket.connected) {
+            if (this.socket.off) this.socket.off();
+            if (this.socket.destroy) this.socket.destroy();
+            delete this.socket;
+            this.connect(fn);
+        }
+    }
+
+    private authenticate(): void {
         let authorization = this.auth.getAuthorization();
         let userId = this.auth.getUserData('id');
-
-        this.socket = io(this._URL_SOCKET);
-        this.socket.on('connect', () => {
-            this.socket.emit('authentication', {
-                id: authorization,
-                userId: userId
-            });
-            this.socket.on('authenticated', () => {
-                console.log('User is authenticated!!!');
-            });
+        this.socket.emit('authentication', {
+            id: authorization,
+            userId: userId
         });
     }
 }
