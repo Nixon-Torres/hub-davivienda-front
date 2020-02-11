8fb623887a22f44c1227679e34e8620d17c13db1
Jeyson Orrego <jeysonorrego@qdata.io>
2020-02-11 10:11:43 -0500

    [HUB-17183] integrate backend -> front

--- src/app/pages/private/board/pdf-upload/pdf-upload.component.ts
+++ src/app/pages/private/board/pdf-upload/pdf-upload.component.ts
@@ -1,15 +1,53 @@
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef } from '@angular/material/dialog';
+
+import { HttpService } from '../../../../services/http.service';
 
 @Component({
-  selector: 'app-pdf-upload',
-  templateUrl: './pdf-upload.component.html',
-  styleUrls: ['./pdf-upload.component.scss']
+    selector: 'app-pdf-upload',
+    templateUrl: './pdf-upload.component.html',
+    styleUrls: ['./pdf-upload.component.scss']
 })
 export class PdfUploadComponent implements OnInit {
+    public uploadForm: FormGroup;
+    public fileData: any = null;
+    public spinner: boolean;
+    public errorMsg: string;
 
-  constructor() { }
+    constructor(
+        public dialogRef: MatDialogRef<PdfUploadComponent>,
+        private formBuilder: FormBuilder,
+        private http: HttpService
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.uploadForm = this.formBuilder.group({
+            file: ['']
+        });
+    }
 
+    onFileSelect(event: any) {
+        if (event.target.files.length > 0) {
+            const file = event.target.files[0];
+            this.uploadForm.get('file').setValue(file);
+        }
+    }
+
+    onSubmit() {
+        const formData = new FormData();
+        formData.append('file', this.uploadForm.get('file').value);
+        this.spinner = true;
+        this.http.post({
+            path: 'upload',
+            data: formData
+        }).subscribe((response: any) => {
+            this.fileData = response.body;
+            this.spinner = false;
+        });
+    }
+
+    closeDialog(data: any): void {
+        this.dialogRef.close(data);
+    }
 }
