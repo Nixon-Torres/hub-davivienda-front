5cdd98ba48a97590efd2b763ee7e6a0590fdce1d
Sergio Niño <sergioanino@qdata.io>
2020-03-27 18:55:53 -0500

    [HUB-17626] Create faq content component

---
+++ src/app/pages/private/principal/faq-content/faq-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-faq-content',
+  templateUrl: './faq-content.component.html',
+  styleUrls: ['./faq-content.component.scss']
+})
+export class FaqContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
