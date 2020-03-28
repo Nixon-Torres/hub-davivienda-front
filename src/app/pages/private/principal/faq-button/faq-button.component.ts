9bdfa2dee06c59096ac8c81f1804d7bd480f7426
Sergio Niño <sergioanino@qdata.io>
2020-03-27 19:57:28 -0500

    [HUB-17626] Add navigation to faq content

--- src/app/pages/private/principal/faq-button/faq-button.component.ts
+++ src/app/pages/private/principal/faq-button/faq-button.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-faq-button',
-  templateUrl: './faq-button.component.html',
-  styleUrls: ['./faq-button.component.scss']
+    selector: 'app-faq-button',
+    templateUrl: './faq-button.component.html',
+    styleUrls: ['./faq-button.component.scss']
 })
 export class FaqButtonComponent implements OnInit {
+    @Output() changeView: EventEmitter<object>;
 
-  constructor() { }
+    constructor() {
+        this.changeView = new EventEmitter(true);
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    public onChangeView(): void {
+        this.changeView.emit({
+            reports: false,
+            editSite: false,
+            faq: true
+        });
+    }
 }
--- src/app/pages/private/principal/faq-content/faq-content.component.ts
+++ src/app/pages/private/principal/faq-content/faq-content.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AuthService} from '../../../../services/auth.service';
 
 @Component({
   selector: 'app-faq-content',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faq-content.component.scss']
 })
 export class FaqContentComponent implements OnInit {
+    @Output() changeView: EventEmitter<object>;
+    public marketing: boolean;
 
-  constructor() { }
+  constructor(
+      private auth: AuthService
+  ) {
+      this.changeView = new EventEmitter<object>();
+      this.marketing = this.auth.isMarketing();
+  }
 
   ngOnInit() {
+  }
+
+  onChangeView() {
+      this.changeView.emit({
+          reports: true,
+          editSite: false,
+          faq: false
+      });
   }
 
 }
