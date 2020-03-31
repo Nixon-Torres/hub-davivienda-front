7046780e50b36ead6bdc149c4696ef288c79b268
Sergio Niño <sergioanino@qdata.io>
2020-03-31 09:58:38 -0500

    [HUB-17544] add multimedia list html and styles

--- src/app/pages/private/principal/multimedia/multimedia.component.ts
+++ src/app/pages/private/principal/multimedia/multimedia.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {AuthService} from '../../../../services/auth.service';
 
 @Component({
-  selector: 'app-multimedia',
-  templateUrl: './multimedia.component.html',
-  styleUrls: ['./multimedia.component.scss']
+    selector: 'app-multimedia',
+    templateUrl: './multimedia.component.html',
+    styleUrls: ['./multimedia.component.scss']
 })
 export class MultimediaComponent implements OnInit {
+    @Output() changeView: EventEmitter<any>;
+    public filterOptions: any;
+    public marketing: boolean;
 
-  constructor() { }
+    constructor(
+        private auth: AuthService
+    ) {
+        this.marketing = this.auth.isMarketing();
+        this.changeView = new EventEmitter<any>();
+        this.filterOptions = [
+            {
+                name: 'Videos',
+                icon: 'fa-play-circle-g',
+            }, {
+                name: 'Podcast',
+                icon: 'fa-podcast',
+            }, {
+                name: 'Webinar',
+                icon: 'fa-broadcast-tower',
+            }
+        ];
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    changeViewFn() {
+        this.changeView.emit({
+            reports: true,
+            editSite: false,
+            faq: false,
+            multimedia: false
+        });
+    }
 
 }
