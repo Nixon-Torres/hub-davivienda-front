bebc8640cc078d7dbcd124c50b56bb6b87daa756
Raj Patel <rajpatel@qdata.io>
2020-04-20 18:26:40 +0530

    Change user permission for copy data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  URL_API: 'http://testhub.qdata.io/api/',
+  URL_API: 'https://testhub.qdata.io/api/',
   URL_SOCKET:  'https://testhub.qdata.io/admin/',
   DRAFT_KEY: '5e068d1cb81d1c5f29b62977',
   STORAGE_FILES: 'https://testhub.qdata.io/public/assets/'
