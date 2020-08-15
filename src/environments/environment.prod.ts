ba76d1e70607a5acfbd727bee4336e447993ec8a
Juan Eduardo Delgado <juandelgado@qdata.io>
2020-08-14 19:02:36 -0500

    [HOTFIX] Add storage url base to prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,6 @@ export const environment = {
     URL_API: 'https://stg-hub.qdata.io/api/',
     URL_SOCKET:  'https://stg-hub.qdata.io/',
     DRAFT_KEY: '5e068d1cb81d1c5f29b62977',
-    STORAGE_FILES: 'https://stg-hub.qdata.io/public/assets/'
+    STORAGE_FILES: 'https://stg-hub.qdata.io/public/assets/',
+    STORAGE_URL_BASE: 'https://stg-hub.qdata.io/',
 };
