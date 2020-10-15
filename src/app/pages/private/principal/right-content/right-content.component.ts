4c77080afb3b9988f687b12525b373cc656c2c33
Nixon-Torres <61920191+Nixon-Torres@users.noreply.github.com>
2020-10-15 12:23:25 -0500

    [HUB-20402-pro] Adjust preview popup size

--- src/app/pages/private/principal/right-content/right-content.component.ts
+++ src/app/pages/private/principal/right-content/right-content.component.ts
@@ -250,7 +250,7 @@ export class RightContentComponent implements OnInit {
 
     openDialog(): void {
         const createDialogRef = this.dialog.open(CreateReportDialogComponent, {
-            width: '1500px',
+            width: '80vw',
             data: {
                 folderId: (this.icurrentObj.currentFolder ? this.icurrentObj.currentFolder : false),
                 stateId: '5e068d1cb81d1c5f29b62977'
