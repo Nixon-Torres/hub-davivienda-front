9a5752548c5093c91003ee46e287f7a3da9f00b7
Juan Eduardo Delgado <juandelgado@qdata.io>
2020-10-15 06:37:00 -0500

    [HOTFIX] Fix url socket url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   URL_API: 'https://vision.davivienda.com/api/',
-  URL_SOCKET:  'https://vision.davivienda.com/api/',
+  URL_SOCKET:  'https://vision.davivienda.com/',
   DRAFT_KEY: '5e068d1cb81d1c5f29b62977',
   STORAGE_FILES: 'https://vision.davivienda.com/public/assets/',
   STORAGE_URL: 'https://vision.davivienda.com/',
